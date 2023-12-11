977828298462019b1dd3db4fc3b4e0878d75f8b1
Denis4554 <125457242+Denis4554@users.noreply.github.com>
2023-12-11 12:35:38 +0200

    Add files via upload

---
+++ Restorant Denis 7/Restorant Denis 7/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restorant_Denis_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RBPushach_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            Form2 form2 = new Form2();
+            form.Hide();
+            form2.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
