cbb87ec831bdd8fb027377ad9872209ceefc4e5f
Denis4554 <125457242+Denis4554@users.noreply.github.com>
2023-12-13 11:44:38 +0200

    Add files via upload

---
+++ Restorant-main/Restorant Denis 7/Restorant Denis 7/Form2.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace Restorant_Denis_7
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnNapitki = new System.Windows.Forms.Button();
+            this.btnDesert = new System.Windows.Forms.Button();
+            this.btnPred = new System.Windows.Forms.Button();
+            this.btnSalad = new System.Windows.Forms.Button();
+            this.btnOSN = new System.Windows.Forms.Button();
+            this.grpBoxSalad = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnAddSalad = new System.Windows.Forms.Button();
+            this.txtGreenSalad = new System.Windows.Forms.TextBox();
+            this.txtClassicSalad = new System.Windows.Forms.TextBox();
+            this.txtSaladBuratta = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblGreenSalad = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblClassicSalad = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSaladBurata = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.grpBoxSalad.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.btnNapitki);
+            this.panel1.Controls.Add(this.btnDesert);
+            this.panel1.Controls.Add(this.btnPred);
+            this.panel1.Controls.Add(this.btnSalad);
+            this.panel1.Controls.Add(this.btnOSN);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 487);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 288);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(186, 199);
+            this.listBox1.TabIndex = 5;
+            // 
+            // btnNapitki
+            // 
+            this.btnNapitki.Location = new System.Drawing.Point(51, 229);
+            this.btnNapitki.Name = "btnNapitki";
+            this.btnNapitki.Size = new System.Drawing.Size(75, 23);
+            this.btnNapitki.TabIndex = 4;
+            this.btnNapitki.Text = "Напитки";
+            this.btnNapitki.UseVisualStyleBackColor = true;
+            // 
+            // btnDesert
+            // 
+            this.btnDesert.Location = new System.Drawing.Point(51, 191);
+            this.btnDesert.Name = "btnDesert";
+            this.btnDesert.Size = new System.Drawing.Size(75, 23);
+            this.btnDesert.TabIndex = 3;
+            this.btnDesert.Text = "Десерт";
+            this.btnDesert.UseVisualStyleBackColor = true;
+            // 
+            // btnPred
+            // 
+            this.btnPred.Location = new System.Drawing.Point(51, 113);
+            this.btnPred.Name = "btnPred";
+            this.btnPred.Size = new System.Drawing.Size(75, 23);
+            this.btnPred.TabIndex = 2;
+            this.btnPred.Text = "Предястия";
+            this.btnPred.UseVisualStyleBackColor = true;
+            // 
+            // btnSalad
+            // 
+            this.btnSalad.Location = new System.Drawing.Point(51, 74);
+            this.btnSalad.Name = "btnSalad";
+            this.btnSalad.Size = new System.Drawing.Size(75, 23);
+            this.btnSalad.TabIndex = 0;
+            this.btnSalad.Text = "Салати";
+            this.btnSalad.UseVisualStyleBackColor = true;
+            this.btnSalad.Click += new System.EventHandler(this.btnSalad_Click);
+            // 
+            // btnOSN
+            // 
+            this.btnOSN.Location = new System.Drawing.Point(51, 150);
+            this.btnOSN.Name = "btnOSN";
+            this.btnOSN.Size = new System.Drawing.Size(75, 23);
+            this.btnOSN.TabIndex = 1;
+            this.btnOSN.Text = "Основни";
+            this.btnOSN.UseVisualStyleBackColor = true;
+            // 
+            // grpBoxSalad
+            // 
+            this.grpBoxSalad.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.grpBoxSalad.Controls.Add(this.label12);
+            this.grpBoxSalad.Controls.Add(this.label11);
+            this.grpBoxSalad.Controls.Add(this.label8);
+            this.grpBoxSalad.Controls.Add(this.btnAddSalad);
+            this.grpBoxSalad.Controls.Add(this.txtGreenSalad);
+            this.grpBoxSalad.Controls.Add(this.txtClassicSalad);
+            this.grpBoxSalad.Controls.Add(this.txtSaladBuratta);
+            this.grpBoxSalad.Controls.Add(this.label7);
+            this.grpBoxSalad.Controls.Add(this.lblGreenSalad);
+            this.grpBoxSalad.Controls.Add(this.label5);
+            this.grpBoxSalad.Controls.Add(this.lblClassicSalad);
+            this.grpBoxSalad.Controls.Add(this.label3);
+            this.grpBoxSalad.Controls.Add(this.lblSaladBurata);
+            this.grpBoxSalad.Controls.Add(this.pictureBox3);
+            this.grpBoxSalad.Controls.Add(this.pictureBox2);
+            this.grpBoxSalad.Controls.Add(this.pictureBox1);
+            this.grpBoxSalad.Location = new System.Drawing.Point(182, 0);
+            this.grpBoxSalad.Name = "grpBoxSalad";
+            this.grpBoxSalad.Size = new System.Drawing.Size(589, 487);
+            this.grpBoxSalad.TabIndex = 1;
+            this.grpBoxSalad.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(48, 298);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Брой порции :";
+            // 
+            // btnAddSalad
+            // 
+            this.btnAddSalad.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAddSalad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAddSalad.Location = new System.Drawing.Point(191, 420);
+            this.btnAddSalad.Name = "btnAddSalad";
+            this.btnAddSalad.Size = new System.Drawing.Size(204, 40);
+            this.btnAddSalad.TabIndex = 12;
+            this.btnAddSalad.Text = "Добави към поръчка";
+            this.btnAddSalad.UseVisualStyleBackColor = true;
+            this.btnAddSalad.Click += new System.EventHandler(this.btnAddSalad_Click);
+            // 
+            // txtGreenSalad
+            // 
+            this.txtGreenSalad.Location = new System.Drawing.Point(226, 317);
+            this.txtGreenSalad.Name = "txtGreenSalad";
+            this.txtGreenSalad.Size = new System.Drawing.Size(139, 20);
+            this.txtGreenSalad.TabIndex = 11;
+            // 
+            // txtClassicSalad
+            // 
+            this.txtClassicSalad.Location = new System.Drawing.Point(415, 317);
+            this.txtClassicSalad.Name = "txtClassicSalad";
+            this.txtClassicSalad.Size = new System.Drawing.Size(139, 20);
+            this.txtClassicSalad.TabIndex = 10;
+            this.txtClassicSalad.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtSaladBuratta
+            // 
+            this.txtSaladBuratta.Location = new System.Drawing.Point(28, 317);
+            this.txtSaladBuratta.Name = "txtSaladBuratta";
+            this.txtSaladBuratta.Size = new System.Drawing.Size(139, 20);
+            this.txtSaladBuratta.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(451, 229);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "24лв.";
+            // 
+            // lblGreenSalad
+            // 
+            this.lblGreenSalad.AutoSize = true;
+            this.lblGreenSalad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblGreenSalad.Location = new System.Drawing.Point(196, 194);
+            this.lblGreenSalad.Name = "lblGreenSalad";
+            this.lblGreenSalad.Size = new System.Drawing.Size(179, 20);
+            this.lblGreenSalad.TabIndex = 7;
+            this.lblGreenSalad.Text = "Зелена салата 300 гр.";
+            this.lblGreenSalad.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(76, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "21лв.";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblClassicSalad
+            // 
+            this.lblClassicSalad.AutoSize = true;
+            this.lblClassicSalad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblClassicSalad.Location = new System.Drawing.Point(378, 194);
+            this.lblClassicSalad.Name = "lblClassicSalad";
+            this.lblClassicSalad.Size = new System.Drawing.Size(211, 20);
+            this.lblClassicSalad.TabIndex = 5;
+            this.lblClassicSalad.Text = "Класическа салата 400 гр.\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(269, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "18лв.";
+            // 
+            // lblSaladBurata
+            // 
+            this.lblSaladBurata.AutoSize = true;
+            this.lblSaladBurata.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSaladBurata.Location = new System.Drawing.Point(6, 194);
+            this.lblSaladBurata.Name = "lblSaladBurata";
+            this.lblSaladBurata.Size = new System.Drawing.Size(181, 20);
+            this.lblSaladBurata.TabIndex = 3;
+            this.lblSaladBurata.Text = "Салата бурата  350 гр.\r\n";
+            this.lblSaladBurata.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(212, 47);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(153, 140);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(401, 47);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(153, 140);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(28, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(432, 298);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(97, 16);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Брой порции :";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(246, 298);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(97, 16);
+            this.label12.TabIndex = 17;
+            this.label12.Text = "Брой порции :";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 487);
+            this.Controls.Add(this.grpBoxSalad);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.grpBoxSalad.ResumeLayout(false);
+            this.grpBoxSalad.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOSN;
+        private System.Windows.Forms.Button btnSalad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnNapitki;
+        private System.Windows.Forms.Button btnDesert;
+        private System.Windows.Forms.Button btnPred;
+        private System.Windows.Forms.GroupBox grpBoxSalad;
+        private System.Windows.Forms.TextBox txtClassicSalad;
+        private System.Windows.Forms.TextBox txtSaladBuratta;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblGreenSalad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblClassicSalad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSaladBurata;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnAddSalad;
+        private System.Windows.Forms.TextBox txtGreenSalad;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+    }
+}
---
+++ Restorant-main/Restorant Denis 7/Restorant Denis 7/Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restorant_Denis_7
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            grpBoxSalad.Hide();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalad_Click(object sender, EventArgs e)
+        {
+            grpBoxSalad.Show();
+        }
+        private void btnAddSalad_Click(object sender, EventArgs e)
+        {
+            if (txtSaladBuratta.Text != "0")
+            {
+                listBox1.Items.Add($"{lblSaladBurata.Text} - {txtSaladBuratta.Text}бр.");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
